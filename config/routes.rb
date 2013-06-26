c8a80f3bc0d3e5074834262a66ac76669179d825
Jun Lim <junyuen@gmail.com>
2013-06-26 19:06:38 +0800

    Add Bootstrap, Pages, Landing Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 App::Application.routes.draw do
+
+  root :to => 'pages#index'
+  get 'about' => 'pages#about'
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +53,7 @@ App::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  
 
   # See how all your routes lay out with "rake routes"
 
